dd8d9204f603b2f01fe47a5384cf116588e04e4c
d11416146 <d11416146@gs.takming.edu.tw>
2026-06-24 15:47:23 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class print
+{
+	public static  string str= "";
+	public void printNum()
+	{
+		lock (this)
+		{
+			for(int i = 1; i <= 5; i++)
+			{
+				str += Thread.CurrentThread.Name + "[" + i + "]" + "\n";
+				Thread.Sleep(new Random().Next(5, 20));
+			}
+			str += "\n";
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace finaltest_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread[] tids = new Thread[3];
+
+            print p1 = new print();
+
+            for (int i = 0; i < 3; i++)
+            {
+                tids[i] = new Thread(new ThreadStart(p1.printNum));
+                tids[i].Name = "執行儲" + i + ":";
+            }
+            foreach (Thread t in tids)
+            {
+                t.Start();
+            }
+            Thread.Sleep(500);
+            Console.WriteLine(print.str);
+        }
+    }
+}
+
